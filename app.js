cf3ac6190a742eb2559ac530785bb129ee9d3cb4
Harry <hfitz83@gmail.com>
2018-02-11 15:25:24 +0000

    add offline page

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 const compression = require('compression')();
 const logger = require('morgan')('dev');
 const helmet = require('helmet')();
@@ -10,6 +10,8 @@ const helmet = require('helmet')();
 const app = express();
 const pub = path.join(__dirname, 'public');
 const index = path.join(pub, 'index.html');
+const offline = path.join(pub, 'offline.html');
+const worker = path.join(pub, 'javascripts', 'worker.js');
 
 /**
  * express middleware
@@ -24,8 +26,8 @@ app.use(helmet);
  * Needed to use /worker to enable the worker to use the
  * entire project dir for cache (if necessary)
  */
-app.get('/worker.js', (req, res) => res.sendFile(path.join(pub, 'javascripts', 'worker.js')));
-
+app.get('/worker.js', (req, res) => res.sendFile(worker));
+app.get('/offline', (req, res) => res.sendFile(offline));
 /**
  * This middle must be the last one set up
  * used for react - enables client-side routing
--- public/javascripts/worker.js
+++ public/javascripts/worker.js
@@ -1,13 +1,16 @@
 // change this
-const cacheName = 'react-boilerplate-1';
+const cacheName = 'pwa-boilerplate-1';
 
 // on *, app.js serves our index file
 // we can be lazy and request /index.html
 // this gets redirected to our layout file
 const toCache = [
+  '/',
   '/index.html',
   // bundle contains all of our CSS
   '/public/bundle.js',
+  // for when we're offline
+  '/offline.html',
 ];
 
 /**
@@ -16,20 +19,19 @@ const toCache = [
  */
 const reqPage = request => {
   const { mode, method, headers } = request;
-  const acceptsHTML = headers.get('accept').includes('text/html')
+  const acceptsHTML = headers.get('accept').includes('text/html');
   return (mode === 'navigate' || (method === 'GET' && acceptsHTML));
 };
 
 /**
- * setup a cache and add items 
+ * setup a cache and add items
  */
 self.addEventListener('install', async event => {
-  event.waitUntil(async function() {
+  event.waitUntil((async function installEvent() {
     const cache = await caches.open(cacheName);
     console.log(`Adding ${toCache} to cache.`);
     return cache.addAll(toCache);
-  }()
-  );
+  }()));
 });
 
 /**
@@ -39,12 +41,15 @@ self.addEventListener('install', async event => {
 self.addEventListener('fetch', event => {
   const { request } = event;
   // Prevent the default, and handle the request ourselves.
-  event.respondWith(async function() {
+  event.respondWith(async function fetchEvent() {
     // Try to get the response from a cache.
     const cachedResponse = await caches.match(request, { cacheName });
     if (cachedResponse) return cachedResponse;
     // if we're offline and requesting a page, return the index file. this mimics the express.js setup.
     if (!navigator.onLine && reqPage(request)) return caches.match('index.html');
-    return fetch(event.request);
+    return fetch(event.request).catch(err => {
+      console.log('Fetch failed; returning offline page instead.', err);
+      return caches.match('/offline.html');
+    });
   }());
-}); 
+});
